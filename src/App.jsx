fdc78464d8b4c768a8a2f1ff76d95a5c9c4fa1a6
Victor Turra <victor.florencio@univem.edu.br>
2023-08-23 16:28:56 -0300

    chore: rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import Task from "./components/Task"
 
 function App() {
+
+  const tasks = [
+    { name: "Inicializar o projeto", actPomodoros: 1, totalPomodoros: 3, isFinished: true },
+    { name: "Implementar cabeçalho", actPomodoros: 2, totalPomodoros: 4, isFinished: false },
+  ]
+
   return (
     <div>
       <div>Timer</div>
       <div>
         <button>Adicionar tarefa</button>
         <div>
-          <Task name="Inicializar o projeto" actPomodoros={1} totalPomodoros={3} isFinished />
-          <Task name="Implementar cabeçalho" actPomodoros={2} totalPomodoros={4} />
+          {tasks.map(task => (
+            <Task
+              name={task.name}
+              actPomodoros={task.actPomodoros}
+              totalPomodoros={task.totalPomodoros}
+              isFinished={task.isFinished}
+            />
+          ))}
         </div>
       </div>
     </div>
