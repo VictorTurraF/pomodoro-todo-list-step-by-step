01be2e19b870208b038a6bb92a3204c6c90b8248
Victor Turra <victor.florencio@univem.edu.br>
2023-08-23 16:49:42 -0300

    chore: state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Task from "./components/Task"
 
 function App() {
+  const [taskName, setTaskName] = useState("")
+  const [taskPomodoros, setTaskPomodoros] = useState(0)
 
   const tasks = [
     { name: "Inicializar o projeto", actPomodoros: 1, totalPomodoros: 3, isFinished: true },
     { name: "Implementar cabeçalho", actPomodoros: 2, totalPomodoros: 4, isFinished: false },
   ]
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const { target: [taskNameInput, taskPomodorosInput] } = event
+
+    setTaskName(taskNameInput.value);
+    setTaskPomodoros(taskPomodorosInput.value)
+  }
+
   return (
     <div>
       <div>Timer</div>
       <div>
-        <button>Adicionar tarefa</button>
+        Task name: {taskName} <br/>
+        Task pomodoros: {taskPomodoros}
+
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Nome da tarefa" />
+          <input type="number" placeholder="Total de pomodoros" />
+          <button type="submit">Adicionar tarefa</button>
+        </form>
+        <hr/>
         <div>
           {tasks.map(task => (
             <Task
