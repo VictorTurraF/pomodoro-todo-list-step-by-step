e82df1c14fcfe93937b2a6ec0b28e5c036bc11bf
Victor Turra <victor.florencio@univem.edu.br>
2023-08-24 10:59:56 -0300

    feat: managing list states

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,31 @@ import { useState } from "react";
 import Task from "./components/Task"
 
 function App() {
-  const [taskName, setTaskName] = useState("")
-  const [taskPomodoros, setTaskPomodoros] = useState(0)
-
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { name: "Inicializar o projeto", actPomodoros: 1, totalPomodoros: 3, isFinished: true },
-    { name: "Implementar cabeçalho", actPomodoros: 2, totalPomodoros: 4, isFinished: false },
-  ]
+    { name: "Implementar cabeçalho", actPomodoros: 1, totalPomodoros: 2, isFinished: false },
+    { name: "Implementar rodapé", actPomodoros: 1, totalPomodoros: 1, isFinished: false },
+  ])
 
   function handleSubmit(event) {
     event.preventDefault();
 
     const { target: [taskNameInput, taskPomodorosInput] } = event
 
-    setTaskName(taskNameInput.value);
-    setTaskPomodoros(taskPomodorosInput.value)
+    const newTask = {
+      name: String(taskNameInput.value),
+      actPomodoros: 0,
+      totalPomodoros: Number(taskPomodorosInput.value),
+      isFinished: false,
+    }
+
+    setTasks(prev => [...prev, newTask]);
   }
 
   return (
     <div>
       <div>Timer</div>
       <div>
-        Task name: {taskName} <br/>
-        Task pomodoros: {taskPomodoros}
-
         <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Nome da tarefa" />
           <input type="number" placeholder="Total de pomodoros" />
